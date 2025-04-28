66ffaf4430e516c97a7a76419fd6c650972317a1
sebastbas2310 <165617545+sebastbas2310@users.noreply.github.com>
2025-04-28 15:01:10 -0500

    el cantante del gueto

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(helmet());  
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use("/auth", authRoutes)
 app.use("/worker", workerRoutes)
